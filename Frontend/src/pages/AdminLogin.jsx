cd5d0fd15fd0fdeb31325c8f9331bedaa7b319b0
excelcluborg <excelclub57@gmail.com>
2026-02-12 23:32:57 +0530

    Update AdminLogin.jsx

--- Frontend/src/pages/AdminLogin.jsx
+++ Frontend/src/pages/AdminLogin.jsx
@@ -22,7 +22,7 @@ const AdminLogin = () => {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch('https://club-excel-official-website.onrender.com/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
